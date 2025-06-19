0df250d4479ef8ce57b2289dbb0e032fbcc0b7e7
David3Emmanuel <david3emmanuel@gmail.com>
2025-06-19 21:23:38 +0100

    Add exception filter and validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { AllExceptionsFilter } from './all-exceptions.filter'
+import { Logger, ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap')
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  app.useGlobalFilters(new AllExceptionsFilter())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  const port = process.env.PORT ?? 3000
+  await app.listen(port)
+  logger.log(`Application is running on port ${port}`)
 }
 
 bootstrap().catch((error) => {
